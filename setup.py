638f6061ca0793f2a89f20c4bbf7b5df3481b10d
trabalhosuft <leonardorec1@gmail.com>
2018-12-17 23:09:46 -0300

    creates the module and add the first func: 'extrai_feature'

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Rafael\'s Feature Handler',
+    url='https://github.com/LeonardoRez/fss-feature-hndlr',
+    author='Leonardo Rezende',
+    author_email='leonardorec1@gmail.com',
+    packages=['features_handler'],
+    install_requires=['numpy'],
+    version='0.1',
+    # license='MIT',
+    description='Functions for handle the features extracted by Rafael Lima on Identification system for smart homes using footstep sounds',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
